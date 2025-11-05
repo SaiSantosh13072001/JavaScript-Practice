0c0b1eb1db063337109ef2358b3cf384e5ecb68b
Sai Santosh Panigrahi <153276610+SaiSantosh13072001@users.noreply.github.com>
2025-11-05 06:57:18 +0000

    Datatypes & variables Details

---
+++ basics/Datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let Name = "Sai Santosh"
+let age = 24
+let isLoggedIn = false
+
+
+// number => 2 to power 52
+// string
+// bigint
+// boolean
+// null => standalone
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof Name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
---
+++ basics/Variable.js
@@ -0,0 +1,12 @@
+const accountId = 11445635
+let accountEmail = "sai@3456gmail.com"
+var accountPassword ="459937"
+accountCity = "Hyderabad"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
